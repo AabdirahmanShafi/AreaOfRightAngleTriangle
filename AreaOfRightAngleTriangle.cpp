5d81aa2e9197bf2432a41b5c107f1a2170f546d9
AabdirahmanShafi <117932483+AabdirahmanShafi@users.noreply.github.com>
2022-11-10 20:03:47 +0300

    Add files via upload

---
+++ AreaOfRightAngleTriangle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int base, height, hypotenuse;
+    float area;
+    cout<<"\nEnter base of right angle triangle:";
+    cin>>base;
+    cout<<"\nEnter height of a riaght angle traingle:";
+    cin>>height;
+    cout<<"\nEnter the hypotenuse of a riaght angle traingle: ";
+    cin>>hypotenuse;
+    area =0.5* base * height;
+    cout<<"\n\nArea of Right angle triangle";
+}
