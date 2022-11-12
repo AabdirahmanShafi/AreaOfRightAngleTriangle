065f26a3820cd83af298d2ac14929994d21214e6
AabdirahmanShafi <117932483+AabdirahmanShafi@users.noreply.github.com>
2022-11-12 12:46:46 +0300

    Update AreaOfRightAngleTriangle.cpp

--- AreaOfRightAngleTriangle.cpp
+++ AreaOfRightAngleTriangle.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int base, height, hypotenuse;
+    int base, height, hypotenuse, perimeter;
     float area;
     cout<<"\nEnter base of right angle triangle:";
     cin>>base;
@@ -11,5 +11,10 @@ int main(){
     cout<<"\nEnter the hypotenuse of a riaght angle traingle: ";
     cin>>hypotenuse;
     area =0.5* base * height;
-    cout<<"\n\nArea of Right angle triangle"<<area<<endl;
+    cout<<"\n\nArea of Right angle triangle is="<<area<<endl;
+
+    perimeter=2*(base+height);
+
+    cout<<"Perimeter of Right angle triangle is="<<perimeter<<endl;
+
 }
